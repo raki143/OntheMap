1c94316d0262b9ae3d7903d28b866033f503f83b
Rakesh Kumar <ace.rakesh@gmail.com>
2017-03-02 01:35:03 +0530

    InformationPostingVC UI implemented

---
+++ OntheMap/InformationPostingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InformationPostingViewController.swift
+//  OntheMap
+//
+//  Created by Rakesh Kumar on 02/03/17.
+//  Copyright © 2017 Rakesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class InformationPostingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
